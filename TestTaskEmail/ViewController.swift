0072bd251e745413d75a55d10d3599814e2533ea
MaxPasuta <pasutmaks@gmail.com>
2022-03-21 10:01:28 +0700

    Initial Commit

---
+++ TestTaskEmail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskEmail
+//
+//  Created by Максим Пасюта on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
